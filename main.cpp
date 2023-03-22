2e315dfcf5ec42fef181056738bdbde170e796a6
sahej-dev <sarao_sahej@outlook.com>
2023-03-22 17:47:44 +0530

    add: space invaders rom

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 
-#include "color.h"
-#include "emuGL.h"
-#include "window.h"
-#include "chip8.h"
 #include "emulator.h"
 
 int main(int argc, char * argv[])
 {
 
-    // chip8::Emulator e{15, "./roms/chip8-test-suite.ch8"};
-    chip8::Emulator e{15, "./roms/Brick.ch8"};
+    chip8::Emulator e{15, "./roms/Space Invaders [David Winter].ch8"};
+    // chip8::Emulator e{15, "./roms/Brick.ch8"};
 
     e.run();
 
