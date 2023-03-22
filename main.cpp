ffd35bf422b8f3599492250a10784dbb1c89e12d
sahej-dev <sarao_sahej@outlook.com>
2023-03-22 19:18:16 +0530

    add: scale factory as a variable

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 
 #include "emulator.h"
 
+const unsigned int SCALE_FACTOR = 15;
+
 int main(int argc, char * argv[])
 {
 
-    chip8::Emulator e{15, "./roms/Space Invaders [David Winter].ch8"};
-    // chip8::Emulator e{15, "./roms/Brick.ch8"};
+    chip8::Emulator e{SCALE_FACTOR, "./roms/Space Invaders [David Winter].ch8"};
+    // chip8::Emulator e{SCALE_FACTOR, "./roms/Brick.ch8"};
 
     e.run();
 
